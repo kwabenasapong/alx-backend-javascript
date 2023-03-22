4ea3654953feb62604069fe48016c6a97e9a7704
Kwabena Sapong <kwabenasapong@gmail.com>
2023-03-22 08:52:33 +0000

    Solve task 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,12 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let idx = 0;
+  for (const value of array) {
+    if (idx < array.length) {
+      // eslint-disable-next-line no-param-reassign
+      array[idx] = appendString + value;
+      idx += 1;
+    }
+  }
+
+  return array;
+}
