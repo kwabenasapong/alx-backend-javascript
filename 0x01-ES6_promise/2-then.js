52a5c13cd9099e4d2e49fb2f448c09aa6ea544d3
Kwabena Sapong <kwabenasapong@gmail.com>
2023-03-23 13:31:00 +0000

    Update 2 to solve task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,11 @@
 const trueBool = { status: 200, body: 'Success' };
-// const falseBool = 'The fake API is not working currently';
 
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
+  promise.then((result) => {
     console.log('Got a response from the API');
-    // eslint-disable-next-line no-constant-condition
-    if (promise) {
-      return resolve(trueBool);
-    }
-    return reject(new Error());
+    return result;
+  }).catch((error) => {
+    console.log('Got an error from the API');
+    return error;
   });
 }
