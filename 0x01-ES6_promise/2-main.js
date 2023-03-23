0c90997525d57c785ce7cbc7bbd469e4036654f3
Kwabena Sapong <kwabenasapong@gmail.com>
2023-03-23 16:43:54 +0000

    Update 4 to solve task 2

--- 0x01-ES6_promise/2-main.js
+++ 0x01-ES6_promise/2-main.js
@@ -2,8 +2,3 @@ import handleResponseFromAPI from './2-then';
 
 const promise = Promise.resolve();
 handleResponseFromAPI(promise);
-console.log(handleResponseFromAPI(promise));
-
-const promise1 = Promise.reject();
-handleResponseFromAPI(promise1);
-console.log(handleResponseFromAPI(promise1));
--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,7 @@
+const resolved = { status: 200, body: 'success' };
+
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
+  promise.then(() => resolved).catch((Error) => Error).finally(() => {
     console.log('Got a response from the API');
-    return { status: 200, body: 'Success' };
-  }).catch((Error) => {
-    console.log('Got a response from the API');
-    return Error;
   });
 }
