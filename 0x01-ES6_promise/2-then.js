5b276ecfb2b8a806b6ee5ffc18f92a510aa435ee
Kwabena Sapong <kwabenasapong@gmail.com>
2023-03-24 07:40:37 +0000

    Update 5 to solve task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,21 @@
-const resolved = { status: 200, body: 'success' };
+/* eslint-disable no-unused-vars */
+/* const resolved = { status: 200, body: 'success' };
 
 export default function handleResponseFromAPI(promise) {
   promise.then(() => resolved).catch((Error) => Error).finally(() => {
     console.log('Got a response from the API');
+  });
+} */
+export default function handleResponseFromAPI(promise) {
+  promise.then((result) => {
+    console.log('Got a response from the API');
+    const response = {
+      status: 200,
+      body: 'success',
+    };
+    return response;
+  }).catch((error) => {
+    console.log('Got a response from the API');
+    return new Error();
   });
 }
