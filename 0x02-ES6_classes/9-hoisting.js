39afe81cf0feffe639ba369696853aa6c80d1674
Kwabena Sapong <kwabenasapong@gmail.com>
2023-03-27 21:16:38 +0000

    Solve task 9

---
+++ 0x02-ES6_classes/9-hoisting.js
@@ -0,0 +1,71 @@
+class HolbertonClass {
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
+
+  get year() {
+    return this._year;
+  }
+
+  set year(newYear) {
+    this._year = newYear;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(newLocation) {
+    this._location = newLocation;
+  }
+}
+
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
+
+  get firstName() {
+    return this._firstName;
+  }
+
+  get lastName() {
+    return this._lastName;
+  }
+
+  get holbertonClass() {
+    return this.holbertonClass;
+  }
+
+  set firstName(newFirstName) {
+    this._firstName = newFirstName;
+  }
+
+  set lastName(newLastName) {
+    this._lastName = newLastName;
+  }
+
+  set holbertonClass(newHolbertonClass) {
+    this._holbertonClass = newHolbertonClass;
+  }
+
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
+}
+
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
