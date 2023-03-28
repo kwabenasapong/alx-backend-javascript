7ee2da55356128a660eb981d7b8721335639f35c
Kwabena Sapong <kwabenasapong@gmail.com>
2023-03-28 17:01:47 +0000

    Solve task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const glist = new Map();
+  glist.set('Apples', 10);
+  glist.set('Tomatoes', 10);
+  glist.set('Pasta', 1);
+  glist.set('Rice', 1);
+  glist.set('Banana', 5);
+  return glist;
+}
