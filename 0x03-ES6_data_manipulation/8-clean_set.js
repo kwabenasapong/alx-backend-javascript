bcb8c9084184ce9694f54e30e9b84d3e665ca24f
Kwabena Sapong <kwabenasapong@gmail.com>
2023-03-28 16:10:43 +0000

    Solve task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+/* eslint-disable consistent-return */
+export default function cleanSet(set, startString) {
+  if (startString) {
+    const selected = [...set].filter((value) => value.startsWith(startString));
+    let finalStr = '';
+    for (const str of selected) {
+      const trucStr = str.replace(startString, '');
+      finalStr += `${trucStr}-`;
+    }
+    return finalStr.slice(0, -1);
+  }
+
+  return '';
+}
