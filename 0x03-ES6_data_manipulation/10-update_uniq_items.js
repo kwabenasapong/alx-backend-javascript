4eec918f53cae668cea4242c33bb10f0fda974b2
Kwabena Sapong <kwabenasapong@gmail.com>
2023-03-28 18:59:49 +0000

    solve task 10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(myMap) {
+  if (!(myMap instanceof Map)) {
+    throw Error('Cannot process');
+  }
+  myMap.forEach((value, key) => {
+    if (value === 1) {
+      myMap.set(key, 100);
+    }
+    return myMap;
+  });
+}
