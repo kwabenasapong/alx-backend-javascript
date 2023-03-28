fa9e313388ebc1bcdf41e29e251b765032fbd9e7
Kwabena Sapong <kwabenasapong@gmail.com>
2023-03-28 17:24:27 +0000

    Update 5 for task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,26 +1,13 @@
-// export default function cleanSet(set, startString) {
-//   if (startString !== undefined && startString.length !== 0 && typeof startString === 'string') {
-//     const selected = [...set].filter((value) => value.startsWith(startString));
-//     let finalStr = '';
-//     for (const str of selected) {
-//       const trucStr = str.replace(startString, '');
-//       finalStr += `${trucStr}-`;
-//     }
-//     return finalStr.slice(0, -1);
-//   }
-
-//   return '';
-// }
-function cleanSet(set, startString) {
-  if (startString === undefined || startString.length === 0 || typeof startString !== 'string') {
-    return '';
-  }
-  let result = '';
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      result += `-${value.substring(startString.length)}`;
+export default function cleanSet(set, startString) {
+  if (startString !== undefined && startString.length !== 0 && typeof startString === 'string') {
+    const selected = [...set].filter((value) => (value !== undefined ? value.startsWith(startString) : ''));
+    let finalStr = '';
+    for (const str of selected) {
+      const trucStr = str.replace(startString, '');
+      finalStr += `${trucStr}-`;
     }
+    return finalStr.slice(0, -1);
   }
-  return result.substring(1);
+
+  return '';
 }
-export default cleanSet;
