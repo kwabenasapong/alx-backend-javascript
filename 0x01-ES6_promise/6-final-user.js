320143eb7276c4c6e7ef21c17e70cc859bb08ef8
Kwabena Sapong <kwabenasapong@gmail.com>
2023-04-04 15:47:30 +0000

    Update task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,23 +1,20 @@
+/*
+Import signUpUser from 4-user-promise.js and uploadPhoto from 5-photo-reject.js.
+
+Write and export a function named handleProfileSignup. It should accept three arguments firstName (string), lastName (string), and fileName (string). The function should call the two other functions. When the promises are all settled it should return an array with the following structure:
+
+[
+    {
+      status: status_of_the_promise,
+      value: value or error returned by the Promise
+    },
+    ...
+  ]
+  */
 import signupUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([uploadPhoto(fileName), signupUser(firstName, lastName)])
-    .then((values) => {
-      for (const value of values) {
-        if (value.status === 'rejected') {
-          return [{ status: 'rejected', value: value.reason.message }];
-        }
-      }
-      return values;
-    })
-    .then((values) => {
-      const result = [];
-      for (const value of values) {
-        if (value.status === 'fulfilled') {
-          result.push({ status: 'fulfilled', value: value.value });
-        }
-      }
-      return result;
-    });
+  return Promise.allSettled([signupUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => values.map((value) => ({ status: value.status, value: value.value })));
 }
