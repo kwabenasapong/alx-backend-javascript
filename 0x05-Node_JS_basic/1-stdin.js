ad6154b82d5e10a119b4c1672fec3d4cb4ff6344
Kwabena Sapong <kwabenasapong@gmail.com>
2023-05-22 08:54:55 +0000

    Update task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,23 @@
-process.stdin.setEncoding("utf8");
+//a program named 1-stdin.js that will be executed through command line:
 
-process.stdin.on("readable", () => {
+/* It should display the message Welcome to Holberton School,
+what is your name? (followed by a new line)
+The user should be able to input their name on a new line
+The program should display Your name is: INPUT
+When the user ends the program, it should display
+This important software is now closing (followed by a new line) */
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) {
     process.stdout.write(`Your name is: ${name}`);
   }
 });
 
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
 
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
