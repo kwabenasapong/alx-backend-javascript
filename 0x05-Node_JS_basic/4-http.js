47a5a00001644631a4f9543d161fdb0a16790208
Kwabena Sapong <kwabenasapong@gmail.com>
2023-05-22 10:31:08 +0000

    Solve task 4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,18 @@
+/* In a file named 4-http.js, create a small HTTP
+server using the http module:
+
+It should be assigned to the variable app and
+this one must be exported
+HTTP server should listen on port 1245
+Displays Hello Holberton School! in the page
+body for any endpoint as plain text */
+
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
