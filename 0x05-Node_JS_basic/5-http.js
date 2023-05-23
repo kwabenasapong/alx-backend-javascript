c5696ab8a0c40df0cfd4244125a27421b05deb56
Kwabena Sapong <kwabenasapong@gmail.com>
2023-05-23 13:54:59 +0000

    Update task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -9,21 +9,22 @@ followed by the same content as the file 3-read_file_async.js (with and without 
 - the name of the database must be passed as argument of the file
 CSV file can contain empty lines (at the end) - and they are not a valid student! */
 
-const http = require('http');
-const countStudents = require('./3-read_file_async');
+const http = require("http");
+const countStudents = require("./3-read_file_async");
 
 const app = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    res.write('This is the list of our students\n');
+  if (req.url === "/") {
+    res.end("Hello Holberton School!");
+  } else if (req.url === "/students") {
+    res.write("This is the list of our students\n");
+    //use async function and await to get data from promise
     countStudents(process.argv[2])
       .then((data) => {
         res.end(data);
-      })
+      }) //if promise is resolved, print data
       .catch((error) => {
         res.end(error.message);
-      });
+      }); //if promise is rejected, print error message
   }
 });
 
