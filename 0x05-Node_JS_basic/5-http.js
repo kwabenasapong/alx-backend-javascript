07a0c7184e97535d3b469b9d25b48623cd8d5dfd
Kwabena Sapong <kwabenasapong@gmail.com>
2023-05-29 10:01:44 +0000

    Update task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -7,37 +7,63 @@ When the URL path is /, it should display Hello Holberton School! in the page bo
 When the URL path is /students, it should display This is the list of our students
 followed by the same content as the file 3-read_file_async.js (with and without the database)
 - the name of the database must be passed as argument of the file
-CSV file can contain empty lines (at the end) - and they are not a valid student! */
+CSV file can contain empty lines (at the end) -
+and they are not a valid student! */
 
 const http = require('http');
-// const countStudents = require('./3-read_file_async');
 
-/* Using the database database.csv (provided in project description),
-create a function countStudents in the file 3-read_file_async.js
+const fs = require('fs');
 
-Create a function named countStudents. It should accept a path
-in argument (same as in 2-read_file.js)
-The script should attempt to read the database file asynchronously
-The function should return a Promise
-If the database is not available, it should throw an error with
-the text Cannot load the database
-If the database is available, it should log the following message to
-the console Number of students: NUMBER_OF_STUDENTS
-It should log the number of students in each field, and the list
-with the following format: Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES
-CSV file can contain empty lines (at the end) - and they are not a valid student! */
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      try {
+        if (err) throw new Error('Cannot load the database');
+        const lines = data.split('\n');
+        lines.shift();
+        let info = '';
+        let count = lines.length;
+        if (lines[lines.length - 1] === '') {
+          count -= 1;
+        }
+        const fields = {};
+        for (const line of lines) {
+          if (fields[line.split(',')[3]] === undefined) {
+            fields[line.split(',')[3]] = [];
+          }
+          if (line.split(',')[0] !== 'firstname') {
+            fields[line.split(',')[3]].push(line.split(',')[0]);
+          }
+        }
+        info += `Number of students: ${count}\n`;
+        for (const field of Object.keys(fields)) {
+          if (field) {
+            info += `Number of students in ${field}: ${
+              fields[field].length}.List: ${fields[field].join(', ')}\n`;
+          }
+        }
+        resolve(info);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  });
+}
 
-const countStudents = require('./3-read_file_async');
-
-const app = http.createServer(async (req, res) => {
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('This is the list of our students\n');
-    const data = await countStudents(process.argv[2]);
-    res.end(`${data}`);
+    countStudents(process.argv[2].toString()).then((info) => {
+      const data = info.slice(0, -1);
+      res.end(data);
+    })
+      .catch((error) => {
+        res.end(error.message);
+      });
   }
 });
 
